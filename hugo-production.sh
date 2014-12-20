5c810a94e9cb91be85a81aaba257e4b5c324eecb
Cyrill Schumacher <cyrill@schumacher.fm>
2014-12-20 16:36:44 +1100

    Create Production version

--- hugo-production.sh
+++ hugo-production.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 rm -Rf public
 mkdir public
+cp static/.htaccess public/
 ./hugo -v --baseUrl="http://cyrillschumacher.com//"
 git ca -m 'Create Production version'
 ./syncFolders-push.sh
